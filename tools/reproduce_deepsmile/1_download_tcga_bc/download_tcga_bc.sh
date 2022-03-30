5c055a2619081633fbeb379df3964fa5745124e6
Yoni Schirris <yschirris@gmail.com>
2022-03-30 11:37:41 +0200

    correct gdc manifest filename

--- tools/reproduce_deepsmile/1_download_tcga_bc/download_tcga_bc.sh
+++ tools/reproduce_deepsmile/1_download_tcga_bc/download_tcga_bc.sh
@@ -6,4 +6,4 @@ REL_PATH_TO_RESOURCES=../../../../resources/data/tcga_bc
 
 mkdir -p images && cd images
 
-$REL_PATH_TO_RESOURCES/gdc-client download -m $REL_PATH_TO_RESOURCES/gdc_manifest_all_BRCA_DX-2020-08-05.txt
+$REL_PATH_TO_RESOURCES/gdc-client download -m $REL_PATH_TO_RESOURCES/gdc_manifest_all_BRCA_DX-2020-11-01.txt
